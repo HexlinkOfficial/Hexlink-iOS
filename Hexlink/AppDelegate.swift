56d3249943d810714af25cb43ac68a4a99ea9e3d
Yang Xi <yang@Yangs-MacBook-Pro.local>
2023-04-15 16:36:28 +0800

    remove the FCM related code

--- Hexlink/AppDelegate.swift
+++ Hexlink/AppDelegate.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 import FirebaseCore
-import FirebaseMessaging
-
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -31,14 +29,6 @@ let gcmMessageIDKey = "gcm.Message_ID"
         )
 
         application.registerForRemoteNotifications()
-        
-        Messaging.messaging().token { token, error in
-          if let error = error {
-            print("Error fetching FCM registration token: \(error)")
-          } else if let token = token {
-            print("FCM registration token: \(token)")
-          }
-        }
         
         return true
     }
@@ -64,14 +54,6 @@ let gcmMessageIDKey = "gcm.Message_ID"
       // this callback will not be fired till the user taps on the notification launching the application.
       // TODO: Handle data of notification
 
-      // With swizzling disabled you must let Messaging know about the message, for Analytics
-      // Messaging.messaging().appDidReceiveMessage(userInfo)
-
-      // Print message ID.
-      if let messageID = userInfo[gcmMessageIDKey] {
-        print("Message ID: \(messageID)")
-      }
-
       // Print full message.
       print(userInfo)
 
@@ -79,25 +61,9 @@ let gcmMessageIDKey = "gcm.Message_ID"
     }
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Messaging.messaging().apnsToken = deviceToken
         let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
         let token = tokenParts.joined()
         print("Device Token: \(token)")
-    }
-}
-
-extension AppDelegate : MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-      print("Firebase registration token: \(String(describing: fcmToken))")
-
-      let dataDict: [String: String] = ["token": fcmToken ?? ""]
-      NotificationCenter.default.post(
-        name: Notification.Name("FCMToken"),
-        object: nil,
-        userInfo: dataDict
-      )
-      // TODO: If necessary send token to application server.
-      // Note: This callback is fired at each app startup and whenever a new token is generated.
     }
 }
 
